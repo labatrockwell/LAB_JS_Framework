a426f24373f16952fef29ee95fbd62c558cfb480
robotconscience <brett@robotconscience.com>
2011-09-06 18:27:34 -0400

    finished updating lab.THREE namespace + docs

--- Templates_Examples/THREEJS_Example/js/lab/app/three/Object.js
+++ Templates_Examples/THREEJS_Example/js/lab/app/three/Object.js
@@ -1,6 +1,9 @@
-/*
- lars berg
- */
+/** @namespace LAB.three */
+LAB.three = LAB.three || {};
+
+/** 
+	@constructor 
+*/
 
 LAB.three.Object = function( _renderer, _scene ) {
    
@@ -22,21 +25,42 @@ LAB.three.Object.prototype = {
    
 constructor: LAB.three.Object,
 
+/**
+	@function
+	@public
+*/
 addGeometry: function( _geometry, _material){
    this.labObj.addChild( new THREE.Mesh( _geometry, _material  ) );
 },
 
+/**
+	@function
+	@public
+*/
 addObject: function( _object ){
    this.labObj.addChild( _object );
 },
+
+/**
+	@function
+	@public
+*/
 addChild: function( _child ){
    this.labObj.addChild( _child );
 },
-   
+
+/**
+	@function
+	@public
+*/
 addLight: function( _light ){
    this.scene.addLight( _light );
 },
-   
+
+/**
+	@function
+	@public
+*/ 
 draw: function( _camera, _material, _renderTarget )
 {   
    this.scene.overrideMaterial = _material || null;
@@ -45,13 +69,21 @@ draw: function( _camera, _material, _renderTarget )
                         _renderTarget,
                         this.autoClear );
 },
-   
+
+/**
+	@function
+	@public
+*/
 translate: function( x,y,z ){
    this.labObj.position.x += x;
    this.labObj.position.y += y;
    this.labObj.position.z += z;
 },
    
+/**
+	@function
+	@public
+*/
 setTranslation: function( x,y,z ){
    this.labObj.position.x = x;
    this.labObj.position.y = y;
@@ -64,33 +96,57 @@ setTranslation: function( x,y,z ){
 //   rotVec.setRotationFromMatrix( rotMat );
 //   this.labObj.rotation.addSelf( rotVec );
 //},
-   
+
+/**
+	@function
+	@public
+*/
 setRotation: function( angle, x,y,z ){
    var rotMat = new THREE.Matrix4().setRotationAxis( new THREE.Vector3(x,y,z).normalize(), angle * 0.0174532925 );
    
    this.labObj.rotation.setRotationFromMatrix( rotMat );
 },
    
-   
+/**
+	@function
+	@public
+*/
 scale: function( x, y, z){
    this.labObj.scale.set(this.labObj.scale.x * x,
                        this.labObj.scale.y * y,
                        this.labObj.scale.z * z);
 },
+
+/**
+	@function
+	@public
+*/
 setScale: function( x, y, z){
    this.labObj.scale.set( x, y, z );
 },
    
+/**
+	@function
+	@public
+*/
 multMatrix: function( m ){
   //TODO: we might need to make this an extension of THREE.Object3D so that we can override updateMatrix() which is called by the render  er
 },
    
+/**
+	@function
+	@public
+*/
 pushMatrix: function(){
    this.posStack.push( this.labObj.position );
    this.rotStack.push( this.labObj.rotation );
    this.sclStack.push( this.labObj.scale );
 },
    
+/**
+	@function
+	@public
+*/
 popMatrix: function(){
    if( this.posStack.length ){
    this.labObj.position.copy( this.posStack.pop() );
