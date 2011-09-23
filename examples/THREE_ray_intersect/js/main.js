9a1a2091e0cb04ba4369e4ac1429d595500f000b
lars berg <laserstorm@gmail.com>
2011-09-23 15:43:11 -0400

    metaball_z

--- examples/THREE_ray_intersect/js/main.js
+++ examples/THREE_ray_intersect/js/main.js
@@ -35,8 +35,11 @@ DemoApp = function() {
 		this.registerMouseEvents();
 
 		//cameras
-        camera = new LAB.three.Camera( 35, window.innerWidth / window.innerHeight, .1, 1000 );
-        camera.setToWindowPerspective();
+//        camera = new LAB.three.Camera( 35, window.innerWidth / window.innerHeight, .1, 1000 );
+//        camera.setToWindowPerspective();
+      
+      camera = new THREE.Camera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
+      camera.position.z = 600;
 
 		ambientLight = new THREE.AmbientLight( 0x222222 );
       this.scene.addLight( ambientLight );
@@ -47,8 +50,8 @@ DemoApp = function() {
       
       var geometry = new THREE.SphereGeometry( 10, 20 ,20);
       mesh = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: 0xffffff, wireframe: true } ));//new THREE.MeshNormalMaterial() );
-      mesh.position.set( window.innerWidth/2, window.innerHeight/2, 0);
-      mesh.scale.set( 30, 30, 30 );
+      mesh.position.set(0,0,0);// window.innerWidth/2, window.innerHeight/2, 0);
+      mesh.scale.set( 20, 20, 20 );
       this.scene.addObject( mesh );
       
       projector = new THREE.Projector();
@@ -66,6 +69,7 @@ DemoApp = function() {
       var pointerGeom = new THREE.LatheGeometry( pointerPoints );
       pointer = new THREE.Mesh( pointerGeom, new THREE.MeshNormalMaterial( ) );
       pointer.scale.set( 10, 10, 10 );
+      pointer.rotation.set( 1, 0, 1 );
       this.scene.addObject( pointer );
       
 	}
@@ -74,13 +78,17 @@ DemoApp = function() {
 	// ===== UPDATE
 	// ===========================================
 	this.update = function() {
-      var vector = new THREE.Vector3( 2 * (lastMouse.x/window.innerWidth - .5), 
-                                      2 * (lastMouse.y/window.innerHeight - .5),
+      var vector = new THREE.Vector3( (lastMouse.x/ window.innerWidth ) * 2 - 1,
+                                      - ( lastMouse.y / window.innerHeight ) * 2 + 1,
                                      0.5 );
+      
+//      mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+//      mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+      
       projector.unprojectVector( vector, camera );
       
-      pointer.position.set( lastMouse.x, window.innerHeight - lastMouse.y, 0 );
-      pointer.rotation.set( LAB.degToRad( lastMouse.x), LAB.degToRad( lastMouse.y ), 0 );
+      pointer.position.set( 280, 117, 0);// lastMouse.x, window.innerHeight - lastMouse.y, 0 );
+//      pointer.rotation.set( LAB.degToRad( lastMouse.x), LAB.degToRad( lastMouse.y ), 0 );
 		
       var ray = new THREE.Ray( camera.position, vector.subSelf( camera.position ).normalize() );
       
@@ -88,10 +96,19 @@ DemoApp = function() {
       
       if ( c ) {
          
-         LAB.log( ray );
-         LAB.log( c );
-         LAB.log( camera.position );
-         pointer.position.copy( ray.origin );
+//         LAB.log( ray );
+//         LAB.log( c );
+//         LAB.log( camera.position );
+//         pointer.position.copy( ray.origin );
+//         pointer.position.addSelf( ray.direction .multiplyScalar( 700 ) );
+         var poi = ray.origin.clone().addSelf( ray.direction.clone().multiplyScalar( c.distance ) );
+         poi.z -= camera.position.z;
+         pointer.position.copy( poi );
+//         poi nter.position.copy( ray.direction.multiplyScalar(c.distance) );
+//         pointer.position.multiplyScalar( window.innerHeight/10 );
+         
+         LAB.log( pointer.position );
+         
          
          gl.clearColor( .4, .4, 4.6, 1);
          
